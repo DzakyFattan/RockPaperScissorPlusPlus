aeb7694cc29d986ee21649dc6abdccc1165911a7
tastytypist <nathanael310802@gmail.com>
2022-04-17 20:20:46 +0700

    Add duration attribute

--- src/main/java/com/aetherwars/model/Spell.java
+++ src/main/java/com/aetherwars/model/Spell.java
@@ -1,6 +1,8 @@
 package com.aetherwars.model;
 
 public class Spell extends Card{
+
+    private int duration;
     private int mana;
     private SpellType type;
 
@@ -8,15 +10,21 @@ public class Spell extends Card{
         super();
         this.type = SpellType.PTN;
         this.mana = 1;
+        this.duration = 0;
     }
 
-    public Spell(String name, String description, SpellType type, int mana) {
+    public Spell(String name, String description, SpellType type, int mana, int duration) {
         super(name, description);
         this.type = type;
         this.mana = mana;
+        this.duration = duration;
     }
 
     // setters
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
     public void setMana(int mana) {
         this.mana = mana;
     }
@@ -26,12 +34,16 @@ public class Spell extends Card{
     }
 
     // getters
+    public int getDuration() {
+        return this.duration;
+    }
+
     public int getMana() {
-        return mana;
+        return this.mana;
     }
 
     public SpellType getType() {
-        return type;
+        return this.type;
     }
 
 }
