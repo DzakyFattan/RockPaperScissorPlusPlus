ed3f3167082a7fb745a5fe13c4c99c6a12857eb4
zaydzuhri <zaydzuhri@gmail.com>
2022-04-27 10:57:35 +0700

    push missing battle module

---
+++ src/main/java/com/aetherwars/battle/activeCard.java
@@ -0,0 +1,33 @@
+package com.aetherwars.battle;
+
+import com.aetherwars.slot.CardOnHand;
+
+public class activeCard {
+    private int id;
+    private CardOnHand card;
+    private boolean active;
+
+    public activeCard(int id, Character character) {
+        this.id=id; //id dari 1-5 yang dipilih pada kotak kosong
+        this.card = new CardOnHand(character);
+        this.active = true;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public CardOnHand getCard() {
+        return card;
+    }
+    public void setCard(CardOnHand card) {
+        this.card = card;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive() {
+        this.active = true;
+    }
+    public void setNonActive() {
+        this.active = false;
+    }
+}
